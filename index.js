da1a14356a8cb91f2e63f907cfa3bed2eaaeb8a4
Bunyod <bunyoddjuraev99@gmail.com>
2023-08-16 17:48:09 +0500

    Uploaded

---
+++ index.js
@@ -0,0 +1,23 @@
+////let age;
+//let b = null;
+
+//console.log(a,b);
+
+//console.log(null + 1);
+
+//let a;
+
+// ================
+//Object - array, obj, function
+
+//let array = ['Bunyod, Ahadjon, Sevinch'] //index
+//let obj = {name: 'webbrain', status: "IT center"} //key
+//let function = ()=>{};
+//function test (){}
+//console.log(array);
+//console.log(obj.name);
+
+
+//Homework
+//==============
+
---
+++ index2.js
@@ -0,0 +1,81 @@
+// let aircon = true;
+// let weather = "quyoshli"
+
+// if (weather === 'quyoshli') {console.log("shining");}
+// else if (weather === 'yomgirli') {console.log("raining");}
+// else if (weather === 'bulutli') {console.log("cloudy");}
+// else if (weather === 'qorli') {console.log("snowing");}
+
+// let gpa = 86;
+
+// if (gpa >= 90 && gpa <= 100){
+//     console.log("grand");
+// } else if (gpa >= 80 && gpa < 90) {
+//     console.log("contract");
+// } else if (gpa >= 70 && gpa < 80) {
+//     console.log("supcontract");
+// } else  {
+//     console.log("fail");
+// }
+ 
+//et gpa = 86;
+
+// gpa >= 56 ? console.log("grand") : console.log("fail");
+//console.log( gpa >= 70 ? "grand" : "fail"); //soddalashtirish
+ 
+
+// ========= Homework========
+// let Edge = Chrome; 
+//  if (Edge = "Chrome") {
+//     console.log("okay we support these browsers too");
+//  } else if (Edge = "Firefox") {
+//     console.log("okay we support these browsers too");
+//  } else if (Edge = "Safari") {
+//     console.log("okay we support these browsers too");
+//  } else if (Edge = "Opera") {
+//     console.log("okay we support these browsers too");
+//  } ;
+
+
+// switch (browser) {
+//     case 'Edge' : 
+//     alert("you've got the Edge!" );
+//     break;
+
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari' :
+//     case 'Opera' :
+//         alert( 'Okay we support therse browsers too' );
+//         break;
+
+//         default:
+//          alert( 'We hope that this page looks ok!' )
+// }
+
+// ========= Homework1=======
+
+
+// let browser = 'Opera';
+//  if (browser === 'Edge') {
+//     console.log("You've got the Edge!");
+//  } else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+//      console.log('Okay, we support these browsers too.');
+//  } else {
+//      console.log('We hope that this page looks okay!');
+//  }
+
+ //==============Homework 2===============
+ //for(let i = 0; i <= 100; i = i+ j){
+   
+ // tub sonni chiqarish
+
+for (let i = 0; i <=100; i++) {
+   for(let j = 0; j<=100; j++){
+    if(i/j === i && i/j === 1) {
+        console.log(i);
+    }
+   }
+    
+}
+
